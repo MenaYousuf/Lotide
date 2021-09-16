dc859abbfd7fe4a377a46c557e0fa187532ede5e
Mena Yousuf <menayousuf99@gmail.com>
2021-09-16 02:47:10 +0000

    countLetters with array

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+/*// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Hello", "Hello");
+assertEqual("Mena", "Muna");
+assertEqual(1, 1);
+assertEqual(12, 21);*/
+
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    assertEqual(result['L'], 2);
+    assertEqual(result['H'], 1);
+    if (letter !== " ") { 
+      assertArraysEqual(middle([1, 2, 3]), [2]);
+        console.log(countLetters('LHL'));
+      }
+    } 
+  }
+
+
