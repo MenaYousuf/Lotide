5df3d18c3098cdd663672e22696402869a6b3311
Mena Yousuf <menayousuf99@gmail.com>
2021-09-16 02:13:28 +0000

    Count only with arrays

---
+++ countOnly.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Hello", "Hello");
+assertEqual("Mena", "Muna");
+assertEqual(1, 1);
+assertEqual(12, 21);
+
+
+const countOnly = functions(allItems, itemsToCount){
+
+}
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+module.exports = countOnly;
