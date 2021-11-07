7734731afcc12d19383e30d6db80a6ee5cd16945
MenaYousuf <menayousuf99@gmail.com>
2021-11-07 20:36:53 +0000

    changes made to tail

--- tail.js
+++ tail.js
@@ -1,23 +1,19 @@
+//  const assertEqual = require("./assertEqual")
+
 const tail = function(arr) {
   if (arr === [] || arr.length === 1) {
     return undefined;
   } else {
-    return arr.splice(1, arr.length);
-  }
+     return arr.splice(1, arr.length)
+}
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+console.log(tail([2,54,6,4]));
 
-
-
-assertEqual("Hello", "Hello");
-assertEqual("Mena", "Muna");
-assertEqual(1, 1);
-assertEqual(12, 21);
-
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+//   }
+// };
