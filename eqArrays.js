8ece8bc790566fd628443b6b9d17908c8be8d16b
MenaYousuf <menayousuf99@gmail.com>
2021-11-07 20:45:56 +0000

    changes made to the eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -1,41 +1,30 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual("Hello", "Hello");
-assertEqual("Mena", "Muna");
-assertEqual(1, 1);
-assertEqual(12, 21);
 
 const eqArrays = function(arr1, arr2) {
+  // if the length is not same return false
   if(arr1.length !== arr2.length) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
+    // if the elements are not same inside the array return false
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
       }
+      // if the element in the arr is same then return true
     } else if (arr1[i] === arr2[i]) {
       return true;
     }
   }
   return true;
 }
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+console.log(eqArrays([1,2,7,4,5,6], [1,2,4,5,6]));
 
-
-
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 
